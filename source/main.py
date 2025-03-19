eaeb6eff8dffad0e322677bd73815f9ec4a058f3
jkoehl <jkoehl@students.uni-mainz.de>
2025-03-19 10:38:10 +0100

    test

--- source/main.py
+++ source/main.py
@@ -8,7 +8,7 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.SCALED) # SCALED flag automatically scales screen to highest possible resolution
 clock = pygame.time.Clock()
 running = True
-
+#test
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
