ca85c7d74536b3da2f58d84cb257a7c99bdabe19
rdfaith <Ihre E-Mail-Adressedmenzel@students.uni-mainz.de>
2025-03-20 14:14:56 +0100

    added object_classes back

--- source/object_classes.py
+++ source/object_classes.py
@@ -119,30 +119,30 @@ class Enemy(MovingObject):
         """Is called on collision with player and reduces lives."""
         threshold = 5
 
-        # player can currently run into the enemy and kill them with the bounce back they should experience
-
         if player.velocity.y < 0 and player.rect.bottom <= self.rect.top + threshold:
-            print(player.rect.bottom)
-            print(self.rect.top + threshold)
             # If player jumps on top of it, enemy dies
             game_world.interactable_objects.remove(self)  # Remove enemy from the game
             player.velocity.y = -250
+            player.bounce_velocity_x = 0
+            player.velocity.x = 0
 
         else:
             player.player_lives -= 1
-            player.bounce_velocity_x = self.current_direction * 300
-            player.velocity.y = -250
+            player.bounce_velocity_x = self.current_direction * 250
+            player.velocity.y = -300
 
 
 class Worm(Enemy):
-    def __init__(self, position: pygame.Vector2, image: pygame.Surface, gravity: bool):
-        super().__init__(position, image, gravity)
-        self.speed_x = 40
+    def __init__(self, position: pygame.Vector2):
+        super().__init__(position, pygame.image.load(get_path("assets/test/worm.png")), True)
+        self.speed_x = 30
         self.distance = 0
         self.max_distance = 50
+        self.animator = Animator(pygame.image.load(get_path('assets/test/worm-Sheet.png')), 32, 16, 5, 10)
 
     def update(self, delta: float, game_world):
         self.velocity.x = self.current_direction * self.speed_x
+        self.animator.update()
 
         super().update(delta, game_world)
 
@@ -154,3 +154,8 @@ class Worm(Enemy):
             self.distance = 0
             self.has_collided = False
 
+    def draw(self, screen, camera_pos):
+        position = self.rect.topleft - camera_pos
+        screen.blit(self.animator.get_frame(self.current_direction), position)
+        # Draw hit box, just for debugging:
+        pygame.draw.rect(screen, (255, 0, 0), self.rect.move(-camera_pos), 2)
