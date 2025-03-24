3e0fcc4eae56671541a9255b28655f7e13214d03
Ashylon <Ashylps@gmail.com>
2025-03-24 13:13:33 +0100

    pausing + menu update

--- source/main.py
+++ source/main.py
@@ -6,22 +6,24 @@ import world_generation
 from utils import *
 from constants import *
 from sound_manager import *
+
 import os
 
 # pygame setup
 pg.init()
-
+from menus import *
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),
                              flags=pg.SCALED)  # SCALED flag automatically scales screen to highest possible resolution
 running = True
 delta = 0.0
 menu = True
 game = False
-woaw = True
-gameo = False
+level_unloaded = False
 level_selection = False
+game_over = False
 selected_level = 0  # Index der ausgewählten Option
-FONT = pg.font.Font(None, 30)
+pause_image = pg.image.load(get_path('assets/test/pause_test.png'))
+
 sound_manager = SoundManager()
 optionbutton = pg.Rect(120,70,80,40)
 
@@ -34,37 +36,25 @@ optionbutton = pg.Rect(120,70,80,40)
 
 while running:
 
+    if menu == True:
 
-    while menu:
-
-        sound_manager.play_bg_music("menu")
-        screen.fill((0,0,0))
-        if pg.Rect.collidepoint(optionbutton,pg.mouse.get_pos()) == True:
-            pg.draw.rect(screen,(255,255,255),optionbutton,50)
-        else:
-            pg.draw.rect(screen,(0,255,255),optionbutton,50)
-        pg.display.flip()
-        
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
                 menu = False
                 level_selection = False
-            if pg.Rect.collidepoint(optionbutton, pg.mouse.get_pos()) == True:
-                if event.type == pg.MOUSEBUTTONDOWN:
-                    menu = False
-                    level_selection = True
+            if event.type == pg.KEYDOWN:
+
+                menu = False
+                level_selection = True
+        start_screen(screen)
+
+        
     while level_selection:
         # Menüoptionen
-        screen.fill((0, 0, 0))
         levels: list[str] = [os.path.splitext(f)[0] for f in os.listdir(get_path(MAP_FOLDER)) if
-                             os.path.isfile(os.path.join(get_path(MAP_FOLDER), f))]
-        for i, option in enumerate(levels):
-            color: str = BLUE if i == selected_level else WHITE
-            text = FONT.render(option, True, color)
-            screen.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, 10 + i * 30))
+                            os.path.isfile(os.path.join(get_path(MAP_FOLDER), f))]
+        level_select(screen,levels,selected_level)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -93,24 +83,44 @@ while running:
                 running = False
                 game = False
 
-            if event.type == pg.KEYDOWN and gameo == True:
-                if event.key == pg.K_e:
-                    game_world = world_generation.generate_world(f"assets/levels/{level}.csv",
-                                                                 'assets/sprites/tiles/autotile_tileset.png')
-                    clock = pg.time.Clock()
-                    gameo = False
+            if event.type == pg.KEYDOWN:
+                if level_unloaded == True:
+                    if event.key == pg.K_e:
+                        game_world = world_generation.generate_world(f"assets/levels/{level}.csv",
+                                                                    'assets/sprites/tiles/autotile_tileset.png')
+                        clock = pg.time.Clock()
+                        level_unloaded = False
+                if event.key == pg.K_ESCAPE:
+                    paused = True
+                    clock = clock
+                    while paused:
+                        
+                        
+                        screen.blit(pause_image,(0,0,320,180))
+                        pg.display.flip()
+                        for event in pg.event.get():
+                            if event.type == pg.KEYDOWN:
+                                if event.key == pg.K_ESCAPE:
+                                    paused = False
+                                    clock = pg.time.Clock()
+                            if event.type == pg.QUIT:
+                                running = False
+                                game = False
+                                paused = False
+
+                        
 
         # do updates
         game_world.do_updates(delta)
         if game_world.player.player_lives <= 0:
-            gameo = True
+            game_over = True
 
         #  render
         game_world.do_render(screen)
-        if gameo == True:
+        if game_over == True:
             #pygame.draw.rect(screen,(255,255,255),optionbutton,50)
-            dah = pg.image.load(get_path('assets/sprites/ui/restart.png'))
-            screen.blit(dah, optionbutton)
+            restart_image = pg.image.load(get_path('assets/sprites/ui/restart.png'))
+            screen.blit(restart_image, optionbutton)
 
         # flip() the display to put your work on screen
         pg.display.flip()
