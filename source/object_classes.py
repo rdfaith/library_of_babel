69dae51b40dfb9bf7d18f0a355084004251dbe14
lgirisch <lgirisch@students.uni-mainz.de>
2025-03-26 13:27:16 +0100

    worm animations update

--- source/object_classes.py
+++ source/object_classes.py
@@ -1,5 +1,6 @@
 from source import *
 
+
 class GameObject:
     def __init__(self, position: pg.Vector2, image: pg.Surface, normal: pg.Surface = None, light_source: LightSource = None):
         self.image = image.convert_alpha()  # Sprite image
@@ -261,7 +262,6 @@ class KeyPickUp(MovingObject):
         )
         super().__init__(position.copy(), image, True, light_source=light_source)
 
-
     def on_collide(self, player, game_world) -> None:
         player.on_pickup_key()
         game_world.interactable_objects.remove(self)
@@ -295,8 +295,6 @@ class Enemy(MovingObject):
         super().__init__(position, image, gravity, hitbox_image=hitbox_image)
         self.current_direction = 1
         self.time_until_death = 3.5
-        # self.active_animation = self.alive
-        # self.animator = Animator(self.active_animation)
 
     class State(Enum):
         ALIVE = 1
@@ -345,8 +343,8 @@ class Worm(Enemy):
         self.max_distance = 50
         self.state = self.State.ALIVE
 
-        self.walk = Animation("walk", get_path('assets/sprites/anim/worm_walk.png'), 32, 16, 5, 10)
-        self.dead = Animation("dead", get_path('assets/test/worm_dead-Sheet.png'), 32, 16, 1, 10)
+        self.walk = Animation("walk", get_path('assets/sprites/anim/worm-walk-Sheet.png'), 32, 16, 6, 10)
+        self.dead = Animation("dead", get_path('assets/sprites/anim/worm-dead-Sheet.png'), 32, 16, 1, 10)
 
         self.active_animation = self.walk
         self.animator = Animator(self.active_animation)
@@ -360,12 +358,6 @@ class Worm(Enemy):
                 self.set_animation(self.walk)
             case self.State.DEAD:
                 self.set_animation(self.dead)
-
-    # def set_animation(self, animation):
-    #     if self.active_animation.name != animation.name:
-    #         self.active_animation = animation
-    #         self.animator = Animator(animation)
-    #         self.animator.reset_animation()
 
     def update(self, delta: float, game_world):
 
