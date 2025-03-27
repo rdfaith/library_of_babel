ab5196d978bcec4f355a0762042e64e437bd62ca
rdfaith <Ihre E-Mail-Adressedmenzel@students.uni-mainz.de>
2025-03-27 12:21:01 +0100

    fixes

--- source/constants.py
+++ source/constants.py
@@ -3,7 +3,7 @@ import os
 from source.utils import get_path
 
 # Debug Settings
-DEBUG_MODE = True
+DEBUG_MODE = False
 DEBUG_HITBOXES = False
 LIGHT_DEBUG_MODE = False
 
@@ -132,11 +132,11 @@ SYSTEM_SOUNDS = {
             "squish": pg.mixer.Sound(get_path("assets/sounds/sfx/squish.mp3")),
             "magical_twinkle": pg.mixer.Sound(get_path("assets/sounds/sfx/magical_twinkle.mp3")),
             "wining": pg.mixer.Sound(get_path("assets/sounds/wining.wav")),
-            "disappointed": pg.mixer.Sound(get_path("assets/sounds/disappointed.wav")),
+            "disappointed": pg.mixer.Sound(get_path("assets/sounds/sad_whine.mp3")),
             "new_highscore": pg.mixer.Sound(get_path("assets/sounds/new_highscore.wav")),
-            #"egg_shaking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_shaking.mp3")),
-            # "egg_cracking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_cracking.mp3")),
-            # "egg_blinking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_blinking.mp3")),
+            "egg_shaking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_shaking.mp3")),
+            "egg_cracking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_cracking.mp3")),
+            "egg_blinking": pg.mixer.Sound(get_path("assets/sounds/sfx/egg_animation/egg_blinking.mp3")),
             "idle": None
         }
 BG_MUSIC = {
